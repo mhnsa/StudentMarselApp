3665720d29de5b72937988a6b5bafcf7c272ce60
Marsel Khalikov <mhnsa@mail.ru>
2021-10-10 20:25:59 +0300

    Initial commit

---
+++ src/main/java/com/example/studentmarselapp/StudentMarselAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentmarselapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentMarselAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentMarselAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studentmarselapp/StudentMarselAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentmarselapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentMarselAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
