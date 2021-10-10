112bd0a956a0ccd09ab4880fc3576a45e31eed81
Marsel Khalikov <mhnsa@mail.ru>
2021-10-10 22:53:14 +0300

    First lesson: second commit

--- src/main/java/com/example/studentmarselapp/model/Group.java
+++ src/main/java/com/example/studentmarselapp/model/Group.java
@@ -1,10 +1,21 @@
 package com.example.studentmarselapp.model;
 
+import lombok.*;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
+import java.util.Set;
 
 
 @Entity
 @Table(name = "university_group")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class Group {
 
     @Id
@@ -13,4 +24,23 @@ public class Group {
 
     @Column(name = "name", unique = true, nullable = false)
     private String name;
+
+    @Column(name = "createAt")
+    private LocalDate createAt;
+
+    @OneToMany(mappedBy = "group")
+    Set<Student> students;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Group group = (Group) o;
+        return Objects.equals(id, group.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
--- src/main/java/com/example/studentmarselapp/model/Student.java
+++ src/main/java/com/example/studentmarselapp/model/Student.java
@@ -1,9 +1,18 @@
 package com.example.studentmarselapp.model;
 
+import lombok.*;
+
 import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Objects;
 
 @Entity
 @Table(name = "student")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     @Id
@@ -15,4 +24,24 @@ public class Student {
 
     @Column(name = "surname", nullable = false, unique = true )
     private String surname;
+
+    @Column(name = "createAt")
+    private LocalDate createAt;
+
+    @ManyToOne()
+    @JoinColumn(name = "group_id", foreignKey = @ForeignKey(name = "student_fk_group"))
+    Group group;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
